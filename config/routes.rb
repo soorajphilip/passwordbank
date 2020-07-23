d50c278181f68d919d1adc1448a559c20f99933f
Bijoy Sijo <bijoysijo21@gmail.com>
2020-07-23 21:07:25 +0530

    fixed root issue using scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :passwords
+  # devise_scope :users do
+  #   root 'devise/sessions#new'
+  # end
   devise_for :users
-  # root 'passwords#index'
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
 end
 
